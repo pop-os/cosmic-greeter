b67fd9c1a0c1e8b661d68452fd1555ce9a2fc516
Matus <Shadiness9530@pm.me>
2024-07-24 10:51:35 +0200

    Create a method to display that the battery is missing/it is a desktop computer

--- src/locker.rs
+++ src/locker.rs
@@ -595,9 +595,16 @@ impl cosmic::Application for App {
 
             if let Some((power_icon, power_percent)) = &self.power_info_opt {
                 status_row = status_row.push(iced::widget::row![
-                    widget::icon::from_name(power_icon.clone()),
-                    widget::text(format!("{:.0}%", power_percent)),
+                    widget::text(if power_percent == &0.0 {
+                        format!("System is running from the wall or the battery")
+                    } else {
+                        widget::icon::from_name(power_icon.clone());
+                        format!("{:.0}%", power_percent)
+                    }),
                 ]);
+            }
+            if self.power_info_opt.is_none() {
+              widget::text("System is running straight from wall");  
             }
 
             //TODO: implement these buttons
