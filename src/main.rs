870dcad9280316635b24a93044752135427d6987
Hugo <hugo@pop-os.home>
2025-03-22 17:14:40 -0400

    Switch to Lexopt

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,36 @@
 
 use cosmic_greeter::{greeter, locker};
 
+use lexopt::{Parser, Arg};
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn print_help() {
+    println!("Usage: cosmic-greeter");
+    println!("\nCOSMIC Greeter");
+    println!("A login and lock screen manager designed for the COSMIC desktop environment. \nFor more information, visit the GitHub repository at https://github.com/pop-os/cosmic-greeter.");
+    println!("\nOptions:");
+    println!("  --help     Show this message");
+    println!("  --version  Show the version of cosmic-greeter");
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut parser = Parser::from_env();
+
+    // Parse the arguments
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Arg::Long("help") => {
+                print_help();
+                return Ok(());
+            }
+            Arg::Long("version") => {
+                println!("cosmic-greeter {}", APP_VERSION);
+                return Ok(());
+            }
+            _ => {}
+        }
+    }
+	
     match pwd::Passwd::current_user() {
         Some(current_user) => match current_user.name.as_str() {
             "cosmic-greeter" => greeter::main(),
@@ -11,4 +40,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
         _ => Err("failed to determine current user".into()),
     }
+
 }
+
