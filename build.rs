bdd4bfa646203c1d9651ae46cd84a87c031a4e77
Michael Aaron Murphy <michael@mmurphy.dev>
2025-03-24 19:43:34 +0100

    improv: help text and format

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
 use vergen::EmitBuilder;
 
 fn main() {
-    EmitBuilder::builder()
-        .git_sha(true)
-        .emit()
-        .unwrap();
+    EmitBuilder::builder().git_sha(true).emit().unwrap();
 }
-
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,7 @@
 
 use cosmic_greeter::{greeter, locker};
 
-use lexopt::{Parser, Arg};
-
-fn print_help() {
-    println!(
-        r#"COSMIC Greeter
-A login and lock screen manager designed for the COSMIC desktop environment.
-
-Project home page: https://github.com/pop-os/cosmic-greeter
-
-Options:
-  --help     Show this message
-  --version  Show the version of cosmic-greeter"#
-    );
-}
+use lexopt::{Arg, Parser};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut parser = Parser::from_env();
@@ -25,11 +12,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     while let Some(arg) = parser.next()? {
         match arg {
             Arg::Short('h') | Arg::Long("help") => {
-                print_help();
+                print_help(env!("CARGO_PKG_VERSION"), env!("VERGEN_GIT_SHA"));
                 return Ok(());
             }
             Arg::Short('v') | Arg::Long("version") => {
-                println!("cosmic-greeter {} (git commit {})",
+                println!(
+                    "cosmic-greeter {} (git commit {})",
                     env!("CARGO_PKG_VERSION"),
                     env!("VERGEN_GIT_SHA")
                 );
@@ -38,7 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => {}
         }
     }
-	
+
     match pwd::Passwd::current_user() {
         Some(current_user) => match current_user.name.as_str() {
             "cosmic-greeter" => greeter::main(),
@@ -46,5 +34,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
         _ => Err("failed to determine current user".into()),
     }
+}
 
+fn print_help(version: &str, git_rev: &str) {
+    println!(
+        r#"cosmic-greeter {version} (git commit {git_rev})
+System76 <info@system76.com>
+
+Designed for the COSMIC™ desktop environment, cosmic-greeter is a libcosmic
+frontend for greetd which can be run inside of cosmic-comp.
+
+Project home page: https://github.com/pop-os/cosmic-greeter
+
+Options:
+  -h, --help     Show this message
+  -v, --version  Show the version of cosmic-greeter"#
+    );
 }
