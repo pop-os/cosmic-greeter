5d28707fb4cc33648783d2ad0a978de83b8f8c66
Jeremy Soller <jackpot51@gmail.com>
2025-03-25 15:13:50 -0600

    Fix vergen generation

--- build.rs
+++ build.rs
@@ -1,5 +1,17 @@
-use vergen::EmitBuilder;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Rebuild if i18n files change
+    println!("cargo:rerun-if-changed=i18n");
 
-fn main() {
-    EmitBuilder::builder().git_sha(true).emit().unwrap();
+    // Emit version information (if not cached by just vendor)
+    let mut vergen = vergen::EmitBuilder::builder();
+    println!("cargo:rerun-if-env-changed=VERGEN_GIT_COMMIT_DATE");
+    if std::env::var_os("VERGEN_GIT_COMMIT_DATE").is_none() {
+        vergen.git_commit_date();
+    }
+    println!("cargo:rerun-if-env-changed=VERGEN_GIT_SHA");
+    if std::env::var_os("VERGEN_GIT_SHA").is_none() {
+        vergen.git_sha(false);
+    }
+    vergen.fail_on_error().emit()?;
+    Ok(())
 }
