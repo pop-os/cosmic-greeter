da4524023432c7b2d41792b9badabffd18ff0d2c
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-07-30 21:54:58 +0300

    adjust padding with no locale

--- src/greeter.rs
+++ src/greeter.rs
@@ -579,42 +579,50 @@ impl App {
                 }
             };
 
-
             let layout_code = self
                 .common
                 .active_layouts
                 .get(self.common.current_keyboard_layout)
-                .map(|l| l.description.as_str())
-                .unwrap_or("")
-                .to_uppercase();
-            
-            let space_width = if !layout_code.is_empty() { 4.0 } else { 0.0 };
-            
-            let button_content = widget::row::with_children(vec![
-                widget::icon::from_name("input-keyboard-symbolic").into(),
-                widget::space::horizontal().width(space_width).into(),
-                widget::text(layout_code).into(),
-            ])
-            .align_y(Alignment::Center);
-            
-            let mut input_button = widget::popover(
-                widget::button::custom(button_content)
-                    .padding([10.0, 14.0])
-                    .on_press(Message::DropdownToggle(Dropdown::Keyboard)),
-            )
-            .position(widget::popover::Position::Bottom);
+                .map(|l| l.layout.as_str())
+                .unwrap_or("");
 
-            if matches!(self.dropdown_opt, Some(Dropdown::Keyboard)) {
-                let mut items = Vec::with_capacity(self.common.active_layouts.len());
-                for (i, layout) in self.common.active_layouts.iter().enumerate() {
-                    items.push(menu_checklist(
-                        &layout.description,
-                        i == self.common.current_keyboard_layout,
-                        Message::KeyboardLayout(i),
-                    ));
-                }
-                input_button = input_button.popup(dropdown_menu(items));
+            let mut button_widgets: Vec<Element<_>> = vec![
+                widget::icon::from_name("input-keyboard-symbolic").into()
+            ];
+
+            if !layout_code.is_empty() {
+                button_widgets.push(widget::space::horizontal().width(4.0).into());
+                button_widgets.push(widget::text(layout_code.to_uppercase()).into());
             }
+
+            let button_content = widget::row::with_children(button_widgets)
+                .align_y(Alignment::Center);
+
+            let button = widget::button::custom(button_content)
+                .padding(if layout_code.is_empty() {[12.0, 12.0]} else {[10.0, 14.0]})
+                .on_press(Message::DropdownToggle(Dropdown::Keyboard));
+
+            let input_button = if matches!(self.dropdown_opt, Some(Dropdown::Keyboard)) {
+                let items = self
+                    .common
+                    .active_layouts
+                    .iter()
+                    .enumerate()
+                    .map(|(i, layout)| {
+                        menu_checklist(
+                            &layout.description,
+                            i == self.common.current_keyboard_layout,
+                            Message::KeyboardLayout(i),
+                        )
+                    })
+                    .collect();
+
+                widget::popover(button)
+                    .position(widget::popover::Position::Bottom)
+                    .popup(dropdown_menu(items))
+            } else {
+                widget::popover(button).position(widget::popover::Position::Bottom)
+            };
             let mut user_button = widget::popover(
                 widget::button::custom(widget::icon::from_name("system-users-symbolic"))
                     .padding(12.0)
